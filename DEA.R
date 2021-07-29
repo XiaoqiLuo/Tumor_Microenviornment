05f27632be8cddaa52e581282dd9c50244ad2494
XiaoqiLuo <50292660+XiaoqiLuo@users.noreply.github.com>
2021-07-29 14:52:30 +0800

    Add files via upload

---
+++ DEA.R
@@ -0,0 +1,155 @@
+setwd('D:\\lxq\\快乐学习每一天\\大四\\ESCC项目整理')
+rm(list = ls())
+args<-commandArgs(T)
+#control<-read.csv('./test/control.txt',header = FALSE)[,1]
+control<-read.csv(args[1],header = FALSE)[,1]
+#treat<-read.csv('./test/treat.txt',header = FALSE)[,1]
+treat<-read.csv(args[2],header = FALSE)[,1]
+p<-args[3]
+needed_packages<-c('TCGAbiolinks','SummarizedExperiment',
+                   'edgeR','pheatmap','ggplot2','ggrepel')
+if (length(which(!(needed_packages %in% rownames(installed.packages()))))>0){
+  print('Please install package(s) listed below:')
+  (needed_packages[!(needed_packages %in% rownames(installed.packages()))])
+}
+
+library(TCGAbiolinks)
+library(SummarizedExperiment)
+query.exp <- GDCquery(project = p,
+                      legacy = TRUE,
+                      data.category = "Gene expression",
+                      data.type = "Gene expression quantification",
+                      platform = "Illumina HiSeq",
+                      file.type = "normalized_results",
+                      experimental.strategy = "RNA-Seq")
+GDCdownload(query.exp)
+exp<-GDCprepare(query = query.exp, save = TRUE, 
+                save.filename = "Exp.rda",remove.files.prepared = TRUE)
+query.exp <- GDCquery(project = p,
+                      legacy = TRUE,
+                      data.category = "Gene expression",
+                      data.type = "Gene expression quantification",
+                      platform = "Illumina HiSeq",
+                      file.type = "normalized_results",
+                      experimental.strategy = "RNA-Seq")
+
+print('Data downloaded!')
+##extract partial sample
+subexp<-exp[,which(exp$sample %in% c(control,treat))]
+dim(subexp)
+#grouping
+group<-rep("control",dim(subexp)[2])
+group[which(subexp$sample %in% treat)]<-'treat'
+table(group)
+subexp@colData@listData$analyType<-group
+
+# #preparation of DEG analysis
+idx <- subexp@colData@listData$analyType %in% c("control")
+idx2 <- subexp@colData@listData$analyType %in% c("treat")
+dataPrep <- TCGAanalyze_Preprocessing(object = subexp, cor.cut = 0.6)
+dataNorm <- TCGAanalyze_Normalization(tabDF = dataPrep,
+                                      geneInfo = geneInfo,
+                                      method = "gcContent")
+dataFilt <- TCGAanalyze_Filtering(tabDF = dataNorm,
+                                  qnt.cut = 0.25,
+                                  method='quantile')
+DEA <- TCGAanalyze_DEA(mat1 = dataFilt[,idx],
+                                mat2 = dataFilt[,idx2],
+                                Cond1type = "control",
+                                Cond2type = "treat",method = "glmLRT")
+DEG<-DEA[which((DEA$PValue<0.05) & abs(DEA$logFC)>1),]
+
+colnames(dataFilt)<-substring(colnames(dataFilt),1,16)
+dataFilt<-log2(dataFilt+1)
+if(length(which(list.files(full.names = TRUE) %in% "./result"))!=0){
+  write.table(dataFilt,"./result/RNAExp.txt",sep = '\t',quote = FALSE)
+  }else{
+  dir.create("result")
+  write.table(dataFilt,"./result/RNAExp.txt",sep = '\t',quote = FALSE)
+  }
+if(length(which(list.files(full.names = TRUE) %in% "./result"))!=0){
+  if(length(which(list.files(paste(getwd(),"./result",sep = '')) %in% "DEA"))!=0 ){
+    write.table(DEG,"./result/DEA/DEG.txt",sep = '\t',quote = FALSE)
+  }else{
+    dir.create("./result/DEA")
+    write.table(DEG,"./result/DEA/DEG.txt",sep = '\t',quote = FALSE)
+  }
+}else{
+  dir.create("result")
+  dir.create("./result/DEA")
+  write.table(DEG,"./result/DEA/DEG.txt",sep = '\t',quote = FALSE)
+}
+print('DEA step1 done.')
+print('DEG file saved.')
+library(pheatmap)
+library(ggplot2)
+col_anno<-data.frame(type=rep('control',dim(dataFilt)[2]))
+rownames(col_anno)<-colnames(dataFilt)
+col_anno[which(rownames(col_anno) %in% treat),1]<-'treat'
+hm<-pheatmap(log2(dataFilt[which(rownames(dataFilt) %in% rownames(DEG)),]+1),
+             show_colnames = FALSE,show_rownames = FALSE,
+             scale = 'row',
+             color = colorRampPalette(colors = c("blue","white","red"))(100),
+             annotation_col = col_anno,cluster_cols = TRUE)
+if(length(which(list.files(full.names = TRUE) %in% "./result"))!=0){
+  if(length(which(list.files(paste(getwd(),"./result",sep = '')) %in% "DEA"))!=0 ){
+    ggsave(hm,filename = "./result/DEA/heatmap.png")
+  }else{
+    dir.create("DEA")
+    ggsave(hm,filename = "./result/DEA/heatmap.png")
+  }
+}else{
+  dir.create("result")
+  dir.create("./result/DEA")
+  ggsave(hm,filename = "./result/DEA/heatmap.png")
+}
+print('DEA step2 done.')
+print('heatmap saved.')
+library(ggplot2)
+library(ggrepel)
+
+DEG$threshold = factor(ifelse(DEG$FDR < 0.05 & abs(DEG$logFC) >= 1, ifelse(DEG$logFC>= 1 ,'Up','Down'),'NoSignifi'),levels=c('Up','Down','NoSignifi'))
+volcano<-ggplot(DEG,aes(x=logFC,y=-log10(FDR),color=threshold))+
+  geom_point()+
+  scale_color_manual(values=c("#DC143C","#00008B","#808080"))+
+  theme_bw()+
+  theme(
+    legend.title = element_blank(),
+    panel.grid.major=element_blank(),panel.grid.minor=element_blank()
+  )+
+  ylab('-log10 (p-adj)')+
+  xlab('log2 (FoldChange)')+
+  geom_vline(xintercept=c(-1,1),lty=3,col="black",lwd=0.5) +
+  geom_hline(yintercept = -log10(0.05),lty=3,col="black",lwd=0.5)
+if(length(which(list.files(full.names = TRUE) %in% "./result"))!=0){
+  if(length(which(list.files(paste(getwd(),"./result",sep = '')) %in% "DEA"))!=0 ){
+    ggsave(volcano,filename = "./result/DEA/volcano.png")
+  }else{
+    dir.create("DEA")
+    ggsave(volcano,filename = "./result/DEA/volcano.png")
+  }
+}else{
+  dir.create("result")
+  dir.create("./result/DEA")
+  ggsave(volcano,filename = "./result/DEA/volcano.png")
+}
+print('DEA step3 done.')
+print('volcano saved.')
+
+####stemness##############################################################################
+st<-TCGAanalyze_Stemness(stemSig = PCBC_stemSig,
+                     dataGE = dataFilt, 
+                     annotation = "sampleType")
+
+if(length(which(list.files(full.names = TRUE) %in% "./result"))!=0){
+  if(length(which(list.files(paste(getwd(),"./result",sep = '')) %in% "Stemness"))!=0 ){
+    write.table(st,"./result/Stemness/stemness.txt",sep = '\t',quote = FALSE,row.names = FALSE)
+  }else{
+    dir.create("./result/Stemness")
+    write.table(st,"./result/Stemness/stemness.txt",sep = '\t',quote = FALSE,row.names = FALSE)
+  }
+}else{
+  dir.create("result")
+  dir.create("./result/Stemness")
+  write.table(st,"./result/Stemness/stemness.txt",sep = '\t',quote = FALSE,row.names = FALSE)
+}
---
+++ Immune.R
@@ -0,0 +1,266 @@
+setwd('D:\\lxq\\快乐学习每一天\\大四\\ESCC项目整理')
+####input##################################################################
+args<-commandArgs(T)
+#i='./result/RNAExp.txt' #arg1
+i<-args[1]
+#control<-read.csv('./test/control.txt',header = FALSE)[,1] #arg2
+control<-read.csv(args[2],header = FALSE)[,1]
+#treat<-read.csv('./test/treat.txt',header = FALSE)[,1] #arg3
+treat<-read.csv(args[3],header = FALSE)[,1]
+
+####follow up#############################################################
+download.file('https://tcga-pancan-atlas-hub.s3.us-east-1.amazonaws.com/download/Survival_SupplementalTable_S1_20171025_xena_sp','followup.txt')
+followup<-read.csv('followup.txt',sep = '\t')
+####packages##############################################################
+needed_packages<-c('ggsignif','ggplot2','estimate','xCell','patchwork','survival','survminer')
+if (length(which(!(needed_packages %in% rownames(installed.packages()))))>0){
+  print('Please install package(s) listed below:')
+  (needed_packages[!(needed_packages %in% rownames(installed.packages()))])
+}
+
+####estimate################################################################
+library(estimate)
+if(length(which(list.files(full.names = TRUE) %in% "./result"))!=0){
+  if(length(which(list.files(paste(getwd(),"./result",sep = '')) %in% "Immune"))!=0 ){
+    filterCommonGenes(input.f=i, 
+                      output.f="./result/Immune/genes.gct", 
+                      id="GeneSymbol")
+  }else{
+    dir.create("./result/Immune")
+    filterCommonGenes(input.f=i, 
+                      output.f="./result/Immune/genes.gct", 
+                      id="GeneSymbol")
+  }
+}else{
+  dir.create("result")
+  dir.create("./result/Immune")
+  filterCommonGenes(input.f=i, 
+                    output.f="./result/Immune/genes.gct", 
+                    id="GeneSymbol")
+}
+
+estimateScore(input.ds = "./result/Immune/genes.gct",
+              output.ds="./result/Immune/estimate_score.gct", 
+              platform="illumina")
+unlink('./result/Immune/genes.gct')
+scores=read.table("./result/Immune/estimate_score.gct",skip = 2,header = T)
+rownames(scores)<-scores$NAME
+scores<-scores[,-c(1,2)]
+colnames(scores)<-gsub('[.]','-',colnames(scores))
+if(length(which(list.files(full.names = TRUE) %in% "./result"))!=0){
+  if(length(which(list.files(paste(getwd(),"./result",sep = '')) %in% "Immune"))!=0 ){
+    write.table(scores,'./result/Immune/estimate_score.txt',sep = '\t',quote = FALSE)
+    unlink('./result/Immune/estimate_score.gct')
+  }else{
+    dir.create("./result/Immune")
+    write.table(scores,'./result/Immune/estimate_score.txt',sep = '\t',quote = FALSE)
+    unlink('./result/Immune/estimate_score.gct')
+  }
+}else{
+  dir.create("result")
+  dir.create("./result/Immune")
+  write.table(scores,'./result/Immune/estimate_score.txt',sep = '\t',quote = FALSE)
+  unlink('./result/Immune/estimate_score.gct')
+}
+
+####estimate plot############################################################
+library(ggplot2)
+library(ggsignif)
+scores<-as.data.frame(t(scores))
+scores$group<-rep('control',dim(scores)[1])
+scores$group[which(rownames(scores) %in% treat)]<-'treat'
+compaired <- list(c('control','treat'))
+
+vioGroup<-function(df,xl,yl,fl){
+  ggplot(df,aes_string(x=xl,y=yl,fill=fl))+
+    geom_violin(position = position_dodge(), 
+                alpha=0.8,width=1) + 
+    guides(fill=F)+xlab('')+
+    #facet_wrap(~ ind, scales="free")+
+    theme_bw()+
+    theme(
+      legend.title = element_blank(),
+      panel.grid.major=element_blank(),panel.grid.minor=element_blank()
+    )+#scale_fill_brewer(palette = "PRGn")+
+    theme(legend.position = "bottom")+
+    #geom_boxplot(position = position_dodge(width = 10))+
+    geom_signif(comparisons = compaired,
+                map_signif_level = F,test = t.test)
+}
+stromal<-vioGroup(scores,'group','StromalScore','group')
+immune<-vioGroup(scores,'group','ImmuneScore','group')
+escore<-vioGroup(scores,'group','ESTIMATEScore','group')
+library(patchwork)
+if(length(which(list.files(full.names = TRUE) %in% "./result"))!=0){
+  if(length(which(list.files(paste(getwd(),"./result",sep = '')) %in% "Immune"))!=0 ){
+    ggsave(stromal+immune+escore,filename = "./result/Immune/estimate-ttest.png")
+  }else{
+    dir.create("Immune")
+    ggsave(stromal+immune+escore,filename = "./result/Immune/estimate-ttest.png")
+  }
+}else{
+  dir.create("result")
+  dir.create("./result/Immune")
+  ggsave(stromal+immune+escore,filename = "./result/Immune/estimate-ttest.png")
+}
+####estimate survival#################################################################
+library(survival)
+require(survminer)
+surv<-followup[,c(1,26:33)]
+scores$bcr<-substring(rownames(scores),1,15)
+scores<-merge(scores,surv,by.x='bcr',by.y='sample')
+#args[4]='mean'
+if(args[4]=='mean'){
+  scores$stromalScore<-ifelse(scores$StromalScore<=mean(scores$StromalScore),'L','H')
+  scores$immuneScore<-ifelse(scores$ImmuneScore<=mean(scores$ImmuneScore),'L','H')
+  scores$estimateScore<-ifelse(scores$ESTIMATEScore<=mean(scores$ESTIMATEScore),'L','H')
+}else if(args[4]=='median'){
+  scores$stromalScore<-ifelse(scores$StromalScore<=median(scores$StromalScore),'L','H')
+  scores$immuneScore<-ifelse(scores$ImmuneScore<=median(scores$ImmuneScore),'L','H')
+  scores$estimateScore<-ifelse(scores$ESTIMATEScore<=median(scores$ESTIMATEScore),'L','H')
+}
+#args[5]='OS'
+formulae <- list()
+element<-c("stromalScore","immuneScore","estimateScore")
+formulae <- lapply(element, function(x) as.formula(paste0('Surv(',args[5],'.time',',',args[5],')', " ~",x)))
+fits <- surv_fit(formulae, data = scores)
+p <- ggsurvplot_list(fits,title=c('      StromalScore','      ImmuneScore','      EstimateScore'),
+                     data = scores,
+                     risk.table = FALSE,
+                     pval = TRUE,ylab = "Recurrence Free Probability",
+                     #break.time.by = 500,#legend = c(0.85,0.25),
+                     #ggtheme = theme_minimal(),
+                     legend.title = ""
+)
+estSur<-arrange_ggsurvplots(p[1:length(p)], print = TRUE, ncol = 2, nrow = 2)
+estSur
+if(length(which(list.files(full.names = TRUE) %in% "./result"))!=0){
+  if(length(which(list.files(paste(getwd(),"./result",sep = '')) %in% "Immune"))!=0 ){
+    ggsave(estSur,filename = "./result/Immune/estimate-survival.png")
+  }else{
+    dir.create("Immune")
+    ggsave(estSur,filename = "./result/Immune/estimate-survival.png")
+  }
+}else{
+  dir.create("result")
+  dir.create("./result/Immune")
+  ggsave(estSur,filename = "./result/Immune/estimate-survival.png")
+}
+####xCell#######################################################################
+#devtools::install_github('dviraran/xCell')
+library(xCell)
+library(survival)
+library(survminer)
+xCell<-xCellAnalysis(read.csv(i,sep = '\t',check.names = FALSE), rnaseq = TRUE)
+xCell<-as.data.frame(t(xCell))
+plist<-vector()
+cellttest<-as.data.frame(matrix(ncol = 4,nrow = 64))
+colnames(cellttest)<-c('CellName','ControlMean','TreatMean','PValue')
+for (j in 1:(ncol(xCell)-3)) {
+  cellttest[j,1]<-colnames(xCell)[j]
+  cellttest[j,2:3]<-t.test(xCell[which(rownames(xCell) %in% control),j],xCell[which(rownames(xCell) %in% treat),j])$estimate
+  cellttest[j,4]<-t.test(xCell[which(rownames(xCell) %in% control),j],xCell[which(rownames(xCell) %in% treat),j])$p.value
+}
+
+if(length(which(list.files(full.names = TRUE) %in% "./result"))!=0){
+  if(length(which(list.files(paste(getwd(),"./result",sep = '')) %in% "Immune"))!=0 ){
+    write.table(cellttest,'./result/Immune/xCell_ttest.txt',sep = '\t',quote = FALSE,row.names = FALSE)
+  }else{
+    dir.create("./result/Immune")
+    write.table(cellttest,'./result/Immune/xCell_ttest.txt',sep = '\t',quote = FALSE,row.names = FALSE)
+  }
+}else{
+  dir.create("result")
+  dir.create("./result/Immune")
+  write.table(cellttest,'./result/Immune/xCell_ttest.txt',sep = '\t',quote = FALSE,row.names = FALSE)
+}
+vCell<-stack(xCell)
+vCell$sample<-rep(rownames(xCell),67)
+vCell$group<-'control'
+vCell$group[which(vCell$sample %in% treat)]<-'treat'
+
+if(length(which(list.files(paste(getwd(),"./result/Immune",sep = '')) %in% 'Immune'))==0){
+  dir.create("./result/Immune/xCell-ttest")
+  print("created file.")
+}
+for(k in which(cellttest$PValue<0.05)){
+  v<-vioGroup(vCell[which(vCell$ind==cellttest$CellName[k]),],'group','values','group')
+  ggsave(v,filename = paste0("./result/Immune/xCell-ttest/",cellttest$CellName[k],"-ttest.png"))
+}
+
+#survival
+xCell$bcr<-substring(rownames(xCell),1,15)
+xCell<-merge(xCell,surv,by.x='bcr',by.y='sample')
+rownames(xCell)<-xCell$bcr
+xCell<-xCell[,-1]
+formulae <- list()
+cellname<-colnames(xCell)
+colnames(xCell)<-gsub(' ','',colnames(xCell))
+colnames(xCell)<-gsub('-','',colnames(xCell))
+colnames(xCell)<-gsub('[+]','',colnames(xCell))
+element<-colnames(xCell)[1:64]
+#arg<-'mean'
+a<-xCell
+for (m in 1:(ncol(xCell)-11)) {
+  n1<-mean(xCell[,m])
+  n2<-median(xCell[,m])
+  for(h in 1:nrow(xCell)){
+    if(args[4]=='mean'){
+      xCell[h,m]<-ifelse(xCell[h,m]<=n1,'L','H')
+    }else{
+      n<-median(xCell[,m])
+      xCell[h,m]<-ifelse(xCell[h,m]<=n2,'L','H')
+    }
+  }
+}
+
+formulae <- lapply(element, function(x) as.formula(paste0('Surv(',args[5],'.time',',',args[5],')', " ~",x)))
+#formulae <- lapply(element, function(x) as.formula(paste0('Surv(','OS','.time',',','OS',')', "~",x)))
+fits <- surv_fit(formulae, data = xCell)
+p <- ggsurvplot_list(fits,title=cellname[1:64],
+                     data = xCell,
+                     risk.table = FALSE,
+                     pval = TRUE,ylab = "Recurrence Free Probability",
+                     #break.time.by = 500,#legend = c(0.85,0.25),
+                     #ggtheme = theme_minimal(),
+                     legend.title = ""
+)
+estSur<-arrange_ggsurvplots(p[1:length(p)], print = TRUE, ncol = 2, nrow = 2)
+pdf('./result/Immune/cell-survival.pdf')
+estSur
+dev.off()
+
+####expr##############################################################################
+deg<-strsplit(args[6], ",")[[1]]
+expr<-read.csv(i,sep = '\t',check.names = FALSE)
+if(length(deg[!(deg %in% rownames(expr))])!=0){
+  print(paste(deg[!(deg %in% rownames(expr))],'is not included in expression matrix!'))
+}
+deg<-deg[which(deg %in% rownames(expr))]
+degcor<-expr[which(rownames(expr) %in% deg),]
+degcor<-as.data.frame(t(degcor))
+xCell<-xCellAnalysis(read.csv(i,sep = '\t',check.names = FALSE), rnaseq = TRUE)
+xCell<-as.data.frame(t(xCell))
+degcor<-merge(degcor,xCell,by='row.names')
+theme_set(ggpubr::theme_pubr()+
+            theme(legend.position = "top"))
+for (k in 1:length(deg)) {
+  if(length(which(list.files(paste(getwd(),"./result/Immune",sep = ''))==deg[k]))==0){
+    dir.create(paste0('./result/Immune/',deg[k]))
+  }
+  for (j in (length(deg)+2):(ncol(degcor)-3)) {
+    ci<-ggplot(degcor, aes(x = degcor[,which(colnames(degcor)==deg[k])], y = degcor[,j])) + 
+      geom_point()+
+      geom_smooth(method = "lm", color = "black", fill = "lightgray")+
+      labs(x = colnames(degcor)[k], y = colnames(degcor)[j])+
+           stat_cor(method = "pearson", 
+           label.x = 3, label.y = 30)
+    ggsave(ci,filename = paste0('./result/Immune/',deg[k],'/',deg[k],'-',colnames(degcor)[j],'.png'))
+  }
+}
+
+
+
+
+
+
